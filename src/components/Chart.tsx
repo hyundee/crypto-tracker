7b575f627752cd902cbe5ea70351ab7dd6cd459d
hyundee <hyundee9@gmail.com>
2023-06-27 17:34:16 +0900

    로딩 문구 수정

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { fetchCoinHistory } from "../api";
 import { useOutletContext } from "react-router-dom";
-import ApexChart from "react-apexcharts";
 import { useRecoilValue } from "recoil";
-import { isThemeAtom } from "../atoms";
 import { styled } from "styled-components";
+import ApexChart from "react-apexcharts";
+import { isThemeAtom } from "../atoms";
+import { fetchCoinHistory } from "../api";
 
 interface IHistorical {
   time_open: number;
@@ -34,65 +34,67 @@ const Chart = () => {
   );
   return (
     <>
-      {data?.length !== undefined ? (
-        isLoading ? (
-          <Text>Loading chart...</Text>
-        ) : (
-          <ApexChart
-            type="candlestick"
-            series={
-              [
-                {
-                  data: data.map((coin: IHistorical) => ({
-                    x: coin.time_open,
-                    y: [coin.open, coin.high, coin.low, coin.close],
-                  })),
-                },
-              ] as unknown as number[]
-            }
-            options={{
-              chart: {
-                height: 250,
-                width: 500,
-                background: "transparent",
-                toolbar: {
-                  show: false,
-                },
-              },
-              theme: {
-                mode: isTheme ? "dark" : "light",
-              },
-              stroke: {
-                curve: "smooth",
-                width: 2,
-              },
-              yaxis: {
-                show: true,
-                tooltip: {
-                  enabled: true,
-                },
-              },
-              xaxis: {
-                type: "datetime",
-                labels: {
-                  style: {
-                    colors: isTheme ? "#f5f6fa" : "#2f3640",
-                  },
-                },
-              },
-              plotOptions: {
-                candlestick: {
-                  colors: {
-                    upward: "#4cd137",
-                    downward: "#00a8ff",
-                  },
-                },
-              },
-            }}
-          ></ApexChart>
-        )
+      {isLoading ? (
+        <Text>Loading chart...</Text>
       ) : (
-        <Text>Price data not found</Text>
+        <>
+          {data?.length !== undefined ? (
+            <ApexChart
+              type="candlestick"
+              series={
+                [
+                  {
+                    data: data.map((coin: IHistorical) => ({
+                      x: coin.time_open,
+                      y: [coin.open, coin.high, coin.low, coin.close],
+                    })),
+                  },
+                ] as unknown as number[]
+              }
+              options={{
+                chart: {
+                  height: 250,
+                  width: 500,
+                  background: "transparent",
+                  toolbar: {
+                    show: false,
+                  },
+                },
+                theme: {
+                  mode: isTheme ? "dark" : "light",
+                },
+                stroke: {
+                  curve: "smooth",
+                  width: 2,
+                },
+                yaxis: {
+                  show: true,
+                  tooltip: {
+                    enabled: true,
+                  },
+                },
+                xaxis: {
+                  type: "datetime",
+                  labels: {
+                    style: {
+                      colors: isTheme ? "#f5f6fa" : "#2f3640",
+                    },
+                  },
+                },
+                plotOptions: {
+                  candlestick: {
+                    colors: {
+                      upward: "#4cd137",
+                      downward: "#00a8ff",
+                    },
+                  },
+                },
+              }}
+            ></ApexChart>
+          ) : (
+            <Text>Price data not found</Text>
+          )}
+        </>
       )}
     </>
   );
